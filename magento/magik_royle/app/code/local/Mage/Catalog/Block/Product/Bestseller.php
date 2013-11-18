0a30da9e47e62666864944cb7abf3d8e3bcde78d
tom.chen <tom.chen@smarcloud.com>
2013-11-18 10:42:26 +0800

    magento template:magik_royle

---
+++ magento/magik_royle/app/code/local/Mage/Catalog/Block/Product/Bestseller.php
@@ -0,0 +1,9 @@
+<?php
+class Mage_Catalog_Block_Product_Bestseller extends Mage_Catalog_Block_Product_Abstract
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+} 
